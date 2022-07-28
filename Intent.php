2e9d1871984f52b7b68a7ef38c5ff9d7c4ab1190
Ayush Rawat <ayushrock13@hotmail.co.uk>
2022-07-28 18:56:15 +0100

    Update 3
    Submit button working correctly and questions for each intent has been added
    
    - stuck with delete and edit button
    - styling and images left

--- Intent.php
+++ Intent.php
@@ -1,28 +1,31 @@
 <?php
+
 include 'connect.php';
-//Check for sumbit button hit with post method for all Types
-if (isset($_POST['submit'])) {
 
-    $dateregistered = date("Y-m-d H:i:s");
-    $entry = $_POST["txtBody"];
-    $type = $_POST['type'];
-    
-
-
-      $sql = "insert into `intent` (datetime,entry,type)
-      values('$dateregistered','$entry','$type')";
-      $result = mysqli_query($conn, $sql);
-      if (!$result) {
-          die(mysqli_error($conn));
-      }
+if(isset($_GET["action"]) == TRUE AND $_GET["action"] == "delete"){
+  $entry=$_GET["id"];
+  $sql="DELETE FROM `intent` WHERE `id`". $entry . "'";	
+  $result=$conn->query($sql);
   
+  }
+
+//Check for sumbit button hit with post method for all Types
+if(isset ($_POST['type']) > 0 ){
+	
+  
+  $type=$_POST["type2"];
+  $dateregistered = date("Y-m-d H:i:s");
+  $entry=$_POST["txtBody"];
+  $sql="INSERT INTO " . $dbname . ".intent (datetime, type, entry) VALUES ('$dateregistered','$type','$entry')";
+  $result=$conn->query($sql);
+  }
+  $conn->close();
 
 
+ 
+  ?>
 
 
-}
-
-?>
 
 
 <!doctype html>
@@ -66,70 +69,66 @@ if (isset($_POST['submit'])) {
 				$conn->close();
 
         echo "<h4>Intent History - All Types</h4><br>";					
-					echo "<table>";
-					echo "<tr>";
-					echo "<th>Date / Time</th><th>Entry</th><th>Type</th><th>Action</th></tr>";
-					for($y=0;$y<sizeof($data);$y++){
-						
-						echo "<tr>";
-						echo "<td>" . $data[$y]["datetime"] . "</td><td>" . $data[$y]["entry"] . "</td><td> ". $data[$y]["type"] ."</td><td><a href='intent.php?id=" . $data[$y]["id"] . "&action=delete'>Delete</a></td><td><a href='getIntent.php?id=" . $data[$y]["id"] . "&action=edit'>Edit</a></td></tr>";
-					
-					}
-					echo "</table><br><br>";
-
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>Date / Time</th><th>Entry</th><th>Type</th><th>Action</th></tr>";
+        for($y=0;$y<sizeof($data);$y++){
           
-				
-
-				
-				for($x=0;$x<sizeof($eachIntent);$x++){
-					$z=$x+1;
-					echo "<div class='single_details'>";
-					echo "<div id='spinner'></div>";
-					echo "<h3 id='" . $z . "textHeading'>" . $eachIntent[$x]["intent"] . "</h3><br><br>";
-          echo "<h4>" . $eachIntent[$x]["intent"] . " Goal Journal History </h4><br>";		
-         
-					echo "<table>";
-					echo "<tr>";
-
+          echo "<tr>";
+          echo "<td>" . $data[$y]["datetime"] . "</td><td>" . $data[$y]["entry"] . "</td><td> ". $data[$y]["type"] ."</td><td><a href='intent.php?id=" . $data[$y]["id"] . "&action=delete'>Delete</a></td><td><a href='intent.php?id=" . $data[$y]["id"] . "&action=edit'>Edit</a></td></tr>";
+        
+        }
+        echo "</table><br><br>";
+  
           
+			
+
 				
-					for($y=0;$y<sizeof($data);$y++){
-					
+
+	
+          for($x=0;$x<sizeof($eachIntent);$x++){
+            $z=$x+1;
+            echo "<div class='single_details'>";
+            echo "<div id='spinner'></div>";
+            echo "<h3 id='" . $z . "textHeading'>" . $eachIntent[$x]["intent"] . "</h3><br><br>";
+                                // create table structure
+            echo "<h4>" . $eachIntent[$x]["intent"] . " Goal Journal History </h4><br>";					
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>Date / Time</th><th>Entry</th><th>Action</th></tr>";
+  
            
+
             
-
-
+            for($y=0;$y<sizeof($data);$y++){
+            
+              
+            
             // populate table or indicate no history
             if($data[$y]["type"] == $eachIntent[$x]["intent"]){  // show entry
               echo "<tr>";
-              echo "<th>Date / Time</th><th>Entry</th><th>Action</th></tr>";
-              echo "<td>" . $data[$y]["datetime"] . "</td><td>" . $data[$y]["entry"] . "</td><td><a href='intent.php?id=" . $data[$y]["id"] . "&action=delete'>Delete</a></td><td><a href='Intent.php?id=" . $data[$y]["id"] . "&action=edit'>Edit</a></td></tr>";
-            }
-          
-            
-            }
 
+                echo "<td>" . $data[$y]["datetime"] . "</td><td>" . $data[$y]["entry"] . "</td><td><a href='intent.php?id=" . $data[$y]["id"] . "&action=delete'>Delete</a></td><td><a href='intent.php?id=" . $data[$y]["id"] . "&action=edit'>Edit</a></td></tr>";
+
+            }
+            }
+            
             echo "</table><br><br>";
+
+            echo "<h3 id='" . $z . "textHeading'>" . $eachIntent[$x]["questions"] . "</h3><br><br>";
             // create textarea for new entry along with add button
             echo "<form name='" . $z . "form' id='" . $z . "form' method='post' action='intent.php'>";
-            echo "<input id ='".$z."type' type=hidden name='type' value='" . $eachIntent[$x]['intent'] ."  '>"; 
-            echo "<textarea id='".$z."txtBody' name='txtBody' rows='10' cols='40' ></textarea><br>";
-            echo "<input type='submit' id='" . $z . "submit' name= 'submit' value='Submit Goal Notation'></input><br><br>";
-            
+            echo "<input type=hidden name='type' value='" . $z . "'>"; 
+            echo "<input type=hidden name='type2' value='" . $eachIntent[$x]['intent'] . "'>"; 
+            echo "<textarea id='txtBody' name='txtBody' rows='10' cols='40' ></textarea><br>";
+            echo "<input type='submit' id='" . $z . "txtsubmit' name='" . $z . "txtsubmit' value='Submit Goal Notation'></input><br><br>";
             echo "</form>";
             echo "<br><br>";
             echo "</div>";
-            echo "<pre>";
-print_r($_POST);
-echo "</pre>";
             
-          }		
-
-
-          if($data[$y]["type"]='Health and Fitness'){
-            echo "<th>Date / Time</th><th>Entry</th><th>Action</th></tr>";
-              echo "<td>" . $data[$y]["datetime"] . "</td><td>" . $data[$y]["entry"] . "</td><td><a href='intent.php?id=" . $data[$y]["id"] . "&action=delete'>Delete</a></td><td><a href='Intent.php?id=" . $data[$y]["id"] . "&action=edit'>Edit</a></td></tr>";
-          }
+            
+          }	
+     
 
           
 					
